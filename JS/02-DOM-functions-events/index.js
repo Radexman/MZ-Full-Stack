e4e255e5712af1847782c79955321db89bfcea45
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-27 11:09:07 +0200

    27.04

--- JS/02-DOM-functions-events/index.js
+++ JS/02-DOM-functions-events/index.js
@@ -1,8 +1,21 @@
 const form = document.getElementById('form');
+const numberInput = document.getElementById('number');
 
 const checkValue = (e) => {
 	e.preventDefault();
-	console.log('Form works');
+
+	const number = Number(numberInput.value);
+	let output = document.getElementById('output');
+
+	if (number === 0) {
+		output.textContent = 'Number is 0.';
+	} else if (number > 0) {
+		output.textContent = 'Number is positive.';
+	} else if (number < 0) {
+		output.textContent = 'Number is neative.';
+	} else if (number !== isNaN) {
+		output.textContent = 'Please enter a number.';
+	}
 };
 
-form.addEventListener('click', checkValue);
+form.addEventListener('submit', checkValue);
