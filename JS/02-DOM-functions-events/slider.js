bab5d4ae64347a360f900e96f61229d1a998bbb9
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-30 18:02:23 +0200

    30.04

--- JS/02-DOM-functions-events/slider.js
+++ JS/02-DOM-functions-events/slider.js
@@ -1,4 +1,6 @@
 const slider = document.querySelector('.slider');
+const form = document.querySelector('.form');
+const slides = document.querySelectorAll('.slide');
 let num = Math.floor(Math.random() * 5 + 1);
 
 const changeSlide = () => {
@@ -9,7 +11,22 @@ const changeSlide = () => {
 
 	slider.style.backgroundImage = `url(./images/slide-${num}.jpg)`;
 
+	setTimeout(() => {
+		slider.classList.remove('fade-out');
+	}, 500);
+
 	setTimeout(changeSlide, 5000);
+
+	setTimeout(() => {
+		slider.classList.add('fade-out');
+	}, 4500);
 };
+
+const changeSlideClick = (e) => {
+	console.log(e.target.value);
+	slider.style.backgroundImage = `url(./images/slide-${e.target.value}.jpg)`;
+};
+
+form.addEventListener('click', changeSlideClick);
 
 changeSlide();
