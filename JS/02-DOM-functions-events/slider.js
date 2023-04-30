c3f51f1c04ccc4bfacede8966a5f0dc63941eda6
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-30 17:31:07 +0200

    30.04

--- JS/02-DOM-functions-events/slider.js
+++ JS/02-DOM-functions-events/slider.js
@@ -1,6 +1,15 @@
+const slider = document.querySelector('.slider');
+let num = Math.floor(Math.random() * 5 + 1);
+
 const changeSlide = () => {
-	let num = Math.floor(Math.random() * 5 + 1);
-	console.log(num);
+	num++;
+	if (num > 5) {
+		num = 1;
+	}
+
+	slider.style.backgroundImage = `url(./images/slide-${num}.jpg)`;
+
+	setTimeout(changeSlide, 5000);
 };
 
 changeSlide();
