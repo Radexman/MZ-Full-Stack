3573750233463877a8b852fa56c9bb53ef1cbae4
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-05-05 16:38:01 +0200

    Timer edge case covered

--- HTML/03-friendly-domains-subpages/timer.js
+++ HTML/03-friendly-domains-subpages/timer.js
@@ -2,23 +2,29 @@
 
 let timerEl = document.querySelector('.nav__timer');
 
+const getTime = () => {
+	const date = new Date();
+
+	let hour = date.getHours();
+	hour = hour < 10 ? `0${hour}` : hour;
+
+	let minute = date.getMinutes();
+	minute = minute < 10 ? `0${minute}` : minute;
+
+	let second = date.getSeconds();
+	second = second < 10 ? `0${second}` : second;
+
+	const fullTime = `${hour}:${minute}:${second}`;
+
+	return fullTime;
+};
+
 const displayTime = () => {
+	timerEl.textContent = getTime();
+
 	setInterval(() => {
-		const date = new Date();
-
-		let hour = date.getHours();
-		hour = hour < 10 ? `0${hour}` : hour;
-
-		let minute = date.getMinutes();
-		minute = minute < 10 ? `0${minute}` : minute;
-
-		let second = date.getSeconds();
-		second = second < 10 ? `0${second}` : second;
-
-		const fullTime = `${hour}:${minute}:${second}`;
-
-		timerEl.textContent = fullTime;
+		timerEl.textContent = getTime();
 	}, 1000);
 };
 
-displayTime();
+document.addEventListener('DOMContentLoaded', displayTime);
